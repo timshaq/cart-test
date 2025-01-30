4362ff7d96a57c83dae3412b4c81599d26ded226
Timur Shakirov <shakirov.t.d@corepoint.tech>
2025-01-30 19:37:55 +0500

    add jwt

--- cart/config/bundles.php
+++ cart/config/bundles.php
@@ -7,4 +7,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Koco\Kafka\KocoKafkaBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
