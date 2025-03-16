827eb878af66b84a0e41fede6d8d7374bc0ca544
timshaq <shakirov53timur@gmail.com>
2025-03-16 14:21:05 +0500

    removed commented code

--- cart/src/Repository/ConstantRepository.php
+++ cart/src/Repository/ConstantRepository.php
@@ -15,29 +15,4 @@ class ConstantRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Constant::class);
     }
-
-    //    /**
-    //     * @return Constant[] Returns an array of Constant objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Constant
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
--- cart/src/Repository/ProductMeasurementRepository.php
+++ cart/src/Repository/ProductMeasurementRepository.php
@@ -15,29 +15,4 @@ class ProductMeasurementRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ProductMeasurement::class);
     }
-
-    //    /**
-    //     * @return Measurments[] Returns an array of Measurments objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('m.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Measurments
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
